1ca98907efb36453723f059b724fb5b8dbfb3647
Niklas Salmoukas <niklas@salmoukas.com>
2023-01-24 09:26:02 -0800

    better ref forwarding util

---
+++ library/src/utils/ref-forwarding.tsx
@@ -0,0 +1,38 @@
+import { MutableRefObject, RefCallback, useMemo, useRef } from "react";
+
+// hook to forward a ref to multiple refs
+export function useRefWithForwarding<Value>(
+    initialValue: Value | null,
+    ...refs: (RefCallback<Value> | MutableRefObject<Value | null>)[]
+): {
+    (node: Value | null): void;
+    current: Value | null;
+} {
+    // inner reference needed to initialize the current value on the setter function
+    const innerRef = useRef<Value | null>(initialValue);
+
+    // generate the setter function
+    return useMemo(
+        () => {
+            // the actual forwarding setter function
+            function setRef(node: Value | null) {
+                innerRef.current = node;
+                for (const ref of refs) {
+                    if (typeof ref === "function") {
+                        ref(node);
+                    } else if (ref) {
+                        ref.current = node;
+                    }
+                }
+            }
+
+            // initialize the current value on the setter function
+            setRef.current = innerRef.current;
+
+            // return the setter function
+            return setRef;
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [...refs]
+    );
+}
