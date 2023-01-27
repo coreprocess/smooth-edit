18e47de845527d303d09e05162cd29dc105de447
Niklas Salmoukas <niklas@salmoukas.com>
2023-01-27 01:07:59 -0800

    Refactoring

--- examples/simple/src/ExampleTextInput.tsx
+++ examples/simple/src/ExampleTextInput.tsx
@@ -1,5 +1,6 @@
-import React, { KeyboardEvent, useCallback, useEffect } from "react";
+import React, { useCallback } from "react";
 import { wrapSmoothEditInput } from "smooth-edit";
+import { UnstyledTextareaAutosize } from "unstyled-textarea-autosize";
 import { useRefWithForwarding } from "use-ref-with-forwarding";
 import "./ExampleTextInput.css";
 
@@ -20,48 +21,22 @@ export const ExampleTextInput = wrapSmoothEditInput<{
         contentRef,
     ]);
 
-    // update the div element when the value changes
-    useEffect(() => {
-        if (ref.current && ref.current.innerText !== value) {
-            ref.current.innerText = value;
-        }
-    }, [ref, value]);
-
-    // propagate value changes
-    const onInput = useCallback(() => {
-        if (ref.current) {
-            onChange(ref.current.innerText);
-        }
-    }, [ref, onChange]);
-
-    // prevent formatting shortcuts
-    const onKeyDown = useCallback(
-        (event: KeyboardEvent) =>
-            (event.ctrlKey || event.metaKey) &&
-            !["c", "v", "x", "z", "r", "ArrowLeft", "ArrowRight"].includes(
-                event.key
-            ) &&
-            event.preventDefault(),
-        []
-    );
-
-    // activate edit mode when the div element is focused
+    // activate edit mode when the textarea element is focused
     const onFocus = useCallback(() => {
         activateEditMode();
     }, [activateEditMode]);
 
-    // render the div element
+    // render the unstyled textare element
     return (
-        <div
+        <UnstyledTextareaAutosize
             ref={ref}
             className={`form-text-input ${
                 editMode ? "edit-mode" : "view-mode"
             }`}
-            contentEditable={true}
             spellCheck={false}
-            onInput={onInput}
-            onKeyDown={onKeyDown}
             onFocus={onFocus}
+            value={value}
+            onValueChange={onChange}
         />
     );
 },
